295c88f64b84f740428d396c039dfc27ce02fd64
Adenike <adeyinkaadenike992@gmail.com>
2023-11-21 12:58:51 +0100

    time

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Color from './Constraint/Color';
+import Images from './Constraint/Images';
+import { useFonts } from 'expo-font';
+import { SIZES, FONTS } from './Constraint/Themes';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    // Montserrat: require('./assets/fonts/Yatra-One.ttf'),
+       Black: require('./assets/font/Lato-Black.ttf'),
+       Blackitalic : require('./assets/font/Lato-BlackItalic.ttf'),
+       Bold : require('./assets/font/Lato-Bold.ttf'),
+       Bolditalic : require('./assets/font/Lato-BoldItalic.ttf'),
+       Light : require('./assets/font/Lato-Light.ttf'),
+       LightItalic : require('./assets/font/Lato-LightItalic.ttf'),
+       Regular : require('./assets/font/Lato-Regular.ttf'),
+       Thin : require('./assets/font/Lato-Thin.ttf'),
+       ThinItalic : require('./assets/font/Lato-ThinItalic.ttf'),
+       Bold : require('./assets/font/Lato-Bold.ttf')
+
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
--- Constraint/Images.js
+++ Constraint/Images.js
@@ -1,8 +1,12 @@
 
 const Images = {
     logo: require('../assets/images/logo.png'),
-    background: require('./background.jpg'),
-    icon: require('./icon.png'),
+    backgrounds: require('../assets/images/background1.png'),
+    background: require('../assets/images/background1.png'),
+    backgroundss: require('../assets/images/background3.png'),
+    email: require('../assets/images/email.png'),
+    arrow: require('../assets/images/arraw1.png'),
+    arrows: require('../assets/images/arrow2.png'),
   };
   export default Images;
   
